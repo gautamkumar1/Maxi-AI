632412475fff6cf3af2fd37a5d7363819311b235
gautamkumar1 <gautamraaz936@gmail.com>
2025-07-15 23:43:49 +0530

    created a frist ai-jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { inngest } from "./client";
-
+import { createAgent, openai } from '@inngest/agent-kit';
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: 'codeAgent',
+      system: 'You are an expert next js developer. You write readable, maintainable, and efficient code. You write simple Next.js & React snippets.',
+      model: openai({ model: "gpt-4o", }),
+    });
+    const { output } = await codeAgent.run(
+      `write the following snippet: ${event.data.email}`,
+    );
+    return { output };
   },
 );
