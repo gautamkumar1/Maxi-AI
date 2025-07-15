042f5456f8d27161293e49c4ace511406ff1094f
gautamkumar1 <gautamraaz936@gmail.com>
2025-07-15 22:56:13 +0530

    created a background job

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
-import { caller } from "@/trpc/server";
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
 
 
-export default async function Home() {
-  const data = await caller.createAI({
-    text: "Hello, world!",})
+export default function Home() {
+  const [value, setValue] = useState("");
+  const trpc = useTRPC()
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: (data) => {
+      toast.success("Background job started successfully!");
+    }
+  }))
   return (
    <div>
-      {JSON.stringify(data)}
+    <Input value={value} onChange={(e)=> setValue(e.target.value)} className="w-1/2 mt-10"/>
+    <Button disabled={invoke.isPending} onClick={()=> invoke.mutate({ text: value })}>Invoke Background job</Button>
    </div>
   );
 }
